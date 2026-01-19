bdfb3ae7239df62cc82cf41f2dec73038057cff7
romarta <marta.rokosa@gmail.com>
2026-01-19 20:48:35 +0100

    excel files

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from config import Config
 from services.openweather_api import fetch_weather
+from services.excel_files import save_to_excel, read_excel_files
+from config import Config
 import time
-
-# zapis do excel
-# zapis do db
-# zapis wykresy i interfejs
 
 
 while True:
     weather = fetch_weather()
-    print(weather)
+    save_to_excel(weather, Config.XLSX_PATH)
+    read_excel_files(Config.XLSX_PATH)
+
     time.sleep(5)
+
+
+
+# zapis do db
+# zapis wykresy i interfejs
